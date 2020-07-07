fc1c55decf256f80138e4366bb3b58179865a299
Nati <natan.kamusher@gmail.com>
2020-07-07 23:11:11 +0300

    Created responsive, variable dimensions board

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import _ from 'lodash'
 import './styles/styles.css'
 
-function component() {
-  const element = document.createElement('div');
+document.addEventListener('DOMContentLoaded', () => {
+  function createTitle() {
+    let titleMarkup = `<h1>Let's play Tic Tac Toe</h1>`
+    document.getElementById('root').insertAdjacentHTML('beforeend', titleMarkup);
+  }
+  function createBoard(dimension = 3) {
+    let boardElement = document.createElement('div');
+    boardElement.classList.add('board');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack!!!!'], ' ');
+    for (let i = 0; i < dimension; i++) {
+      let rowElement = document.createElement('div');
+      rowElement.classList.add('board-row');
+      boardElement.appendChild(rowElement);
 
-  return element;
-}
+      for (let i = 0; i < dimension; i++) {
+        let cellElement = document.createElement('a');
+        cellElement.classList.add('row-cell');
+        rowElement.appendChild(cellElement)
+      }
 
-document.body.appendChild(component());
+    }
+    document.getElementById('root').appendChild(boardElement)
+  }
 
+  function createBoard2(dimension = 3) {
+    let boardElement = document.createElement('div');
+    boardElement.classList.add('board');
+    boardElement = document.getElementById('root').appendChild(boardElement)
+
+    let boardClientWidth = boardElement.clientWidth;
+    let boardOffsetWidth = boardElement.offsetWidth * 1.5;
+
+    console.log(`board client width is : ${boardClientWidth}`);
+    console.log(`board offset width is : ${boardOffsetWidth}`);
+    let cellWidth = boardClientWidth / dimension;
+    let cellWidthRelative = boardClientWidth / dimension / boardOffsetWidth * 100;
+    console.log(`cell width is ${cellWidth}%`)
+
+    for (let i = 0; i < dimension ** 2; i++) {
+      let cellElement = document.createElement('a');
+      cellElement.classList.add('cell');
+      cellElement = boardElement.appendChild(cellElement);
+      cellElement.setAttribute("style", `width: ${cellWidthRelative}%; padding-bottom: ${cellWidthRelative}%; transform: translateY(${boardOffsetWidth / cellWidth * 50}%)`)
+    }
+  }
+
+  createTitle();
+  createBoard2(3);
+});
 if (module.hot) {
   module.hot.accept()
 }
